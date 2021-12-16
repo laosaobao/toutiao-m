f29bc805aa313b3dc3ee7810a11115df1715da09
laosaobao <654280831@qq.com>
2021-12-17 01:04:57 +0800

    引入vant

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,16 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <h1>头条</h1>
+  <div class="">
+    <i class="toutiao toutiao-shipin"></i>
+  </div>
+  <van-cell-group>
+  <van-cell title="单元格" value="内容" />
+  <van-cell title="单元格" value="内容" label="描述信息" />
+  </van-cell-group>
   </div>
 </template>
 
 <style lang="less">
-
 </style>
