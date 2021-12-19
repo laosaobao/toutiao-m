19cc107c86d5346c1dd63f3ef700067ab4c0bb69
laosaobao <654280831@qq.com>
2021-12-20 00:20:35 +0800

    12.19晚

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="app">
     <router-view/>
-    <h1>头条</h1>
-  <div class="">
-    <i class="toutiao toutiao-shipin"></i>
-  </div>
-  <van-cell-group>
-  <van-cell title="单元格" value="内容" />
-  <van-cell title="单元格" value="内容" label="描述信息" />
-  </van-cell-group>
   </div>
 </template>
 
---
+++ src/views/login/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="login-container">
+    <van-nav-bar title="标题" />
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="username"
+        name="用户名"
+        label="用户名"
+        placeholder="用户名"
+        :rules="[{ required: true, message: '请填写用户名' }]"
+      />
+      <van-field
+        v-model="password"
+        type="password"
+        name="密码"
+        label="密码"
+        placeholder="密码"
+        :rules="[{ required: true, message: '请填写密码' }]"
+      />
+      <div style="margin: 16px">
+        <van-button round block type="info" native-type="submit"
+          >提交</van-button
+        >
+      </div>
+    </van-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginIndex",
+  components: {},
+  props: {},
+  data() {
+    return {
+      username: "",
+      password: "",
+    };
+  },
+  methon: {
+    onSubmit(values) {
+      console.log("submit", values);
+    }
+  }
+};
+</script>
+
+<style>
+</style>
