3ef7f8eda40cf74c1c2e3a28bd0c1a437453d6d7
laosaobao <654280831@qq.com>
2021-12-29 00:09:05 +0800

    12.29

--- src/views/my/index.vue
+++ src/views/my/index.vue
@@ -6,6 +6,42 @@
         <span class="text">登录 / 注册</span>
       </div>
     </div>
+
+    <div class="header user-info">
+      <div class="base-info">
+        <div class="left">
+          <van-image
+            class="avatar"
+            round
+            fit="cover"
+            src="https://img01.yzcdn.cn/vant/cat.jpeg"
+          />
+          <span>用户名</span>
+        </div>
+        <div class="right">
+          <van-button size="mini" round>编辑资料</van-button>
+        </div>
+      </div>
+      <div class="data-stats">
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">头条</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">关注</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">粉丝</span>
+        </div>
+        <div class="data-item">
+          <span class="count">10</span>
+          <span class="text">获赞</span>
+        </div>
+
+      </div>
+    </div>
   </div>
 </template>
 
@@ -31,14 +67,56 @@ export default {
       flex-direction: column;
       justify-content: center;
       align-items: center;
-      .mobile-img{
+      .mobile-img {
         width: 132px;
         height: 132px;
         margin-bottom: 10px;
       }
-      .text{
+      .text {
         font-size: 28px;
-        color:#fff;
+        color: #fff;
+      }
+    }
+  }
+  .user-info {
+    .base-info {
+      height: 231px;
+
+      padding: 76px 32px 23px;
+      box-sizing: border-box;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      .left {
+        display: flex;
+        align-items: center;
+        .avatar {
+          width: 132px;
+          height: 132px;
+          margin-right: 23px;
+          border: 1px solid #fff;
+        }
+        .name {
+          font-size: 30px;
+          color: #fff;
+        }
+      }
+    }
+    .data-stats {
+      // height: 130px;
+      background-color: #ccc;
+      display: flex;
+      .data-item{
+        height:130px;
+        flex: 1;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        .count{
+          font-size: 36px;
+        }
+        .text{font-size: 23px;}
       }
     }
   }
